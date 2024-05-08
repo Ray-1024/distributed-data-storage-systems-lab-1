c02b8d229f330f2bebecdaf950180850b61233b9
Ray_1024 <skovorodnikovda@mail.ru>
2024-05-08 13:37:57 +0300

    Fix text prompt setting

--- lab.sql
+++ lab.sql
@@ -1,11 +1,11 @@
 \echo 'Введите название схемы: '
 \prompt current_schema_name
-\set lab_schema_name '\'' :current_schema_name '\''
+\set lab_schema_name $$:current_schema_name$$
 
 
 \echo 'Введите текст для поиска: '
 \prompt current_search_text
-\set lab_search_text '\'' :current_search_text '\''
+\set lab_search_text $$:current_search_text$$
 
 
 call search_code_in_schema(:lab_search_text::text, :lab_schema_name::text);
